5a5b011a2190dbb49608f80060bbee7cd16b87e8
jaeukryu <jaeukryu@devsisters.com>
2026-03-18 14:00:06 +0900

    weak 제거

--- core/media/AvfMediaEngine.mm
+++ core/media/AvfMediaEngine.mm
@@ -385,7 +385,7 @@ void AvfMediaEngine::onStatusNotification(void* context)
         delay one frame to invoke [player play] to fix player.timeControlStatus
         maybe AVPlayerTimeControlStatusPaused at first app startup
         */
-        __weak AVPlayer* player = _player;
+        AVPlayer* player = _player;
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
             if (player != nil)
                 [player play];
